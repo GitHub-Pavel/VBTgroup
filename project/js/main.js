1a9ff68e980a5c9f3c2ed2b3feb7c967d6a3ec78
Pavel <pavel.khatkevich2000@gmail.com>
2020-12-31 01:46:18 +0300

    added front page without mobile version

--- project/js/main.js
+++ project/js/main.js
@@ -52,4 +52,34 @@ $(function () {
         $("html, body").animate({ scrollTop: 0 }, 600);
         return false;
     });
+
+    $('.home-slider').slick({
+        lazyLoad: 'ondemand',
+        infinite: true,
+        autoplay: true,
+        autoplaySpeed: 4000,
+        dots: true,
+        arrows: true,
+        appendArrows: $('.home-offer__navigation'),
+        appendDots: $('.home-offer__dots'),
+        nextArrow: '<button type="button" class="slick-next"><svg class="slick-next__icon"><use href="img/icons.svg#align-right"></use></svg></button>',
+        prevArrow: '<button type="button" class="slick-prev"><svg class="slick-prev__icon"><use href="img/icons.svg#align-left"></use></svg></button>'
+    });
+
+    $('.home-offer__slider-all').text($('.home-offer .slick-dots li').length);
+    $('.home-offer__slider-now').text($('.home-slider').slick('slickCurrentSlide') + 1);
+    $('.home-slider').on('afterChange', function(e, slick) {
+        $('.home-offer__slider-now').text($('.home-slider').slick('slickCurrentSlide') + 1);
+        if ( $('.home-offer .slick-current .home-slider__item').data('src') != undefined ) {
+            $('.home-offer .slick-current .home-slider__item').css({
+                'backgroundImage': 'url(' + $('.home-offer .slick-current .home-slider__item').data('src') + ')'
+            });
+            $('.home-offer .slick-current .home-slider__item').removeAttr('data-src');
+        }
+    });
+
+    $('body').on('click', '.modal__close', function(e) {
+        e.preventDefault();
+        $.fancybox.close();
+    });
 })
